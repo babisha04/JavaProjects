e282902c0ce48e90b6b8abadeefe269d0af173fa
babisha04 <babishariya@gmail.com>
2025-09-11 00:11:09 +0530

    ADDED BOOK CLASS

--- Library.java
+++ Library.java
@@ -12,7 +12,25 @@ public void addbook(Book book){
     books.add(book);
     System.out.println("Book Added"+book.getTitle());
 
+}
+public void removebook(int id){
+    Book target=null;
+    for(Book b:books){
+        if(b.getid()==id){
+            target=b;
+            break;
+        }
+    }
+    if(target==null){
+        System.out.println("No Book Available");
 
+    }
+    if(!target.isAvailable()){
+        System.out.println("Cannot Delete: Its in issued status");
+
+    }
+    books.remove(target);
+    System.out.println("Book"+id+"removed successfully");
 }
 
 
